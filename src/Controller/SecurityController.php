197ecbcdf3ddf2fe8fb5362b28c087ed17f01557
hakim-boop <hh.infographie@gmail.com>
2022-09-05 15:36:56 +0200

    Register + Authentication

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\User;
+use App\Form\RegisterFormType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    // pas index: string no index, index name: 'string', index methods: [array de 'string',]
+    #[Route(path: '/inscription', name: 'app_register', methods: ['GET', 'POST'])]
+    public function register(Request $request, UserRepository $Repository, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegisterFormType::class, $user)
+            ->handleRequest($request);
+
+        if ($form->isSubmitted()  && $form->isValid()) {
+            $user->setCreatedAt(new DateTime());
+            $user->setUpdatedAt(new DateTime());
+            $user->setRoles(["ROLE_USER"]);
+
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $user->getPassword())
+            );
+
+            //Cette ligne fait un persist() + flush()
+            $Repository->add($user, true);
+
+            $this->addFlash('success', "Vous vous êtes inscrit a");
+        }
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/RegisterFormType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+class RegisterFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Choisisser un email',
+                'constraints' => [
+                    new Email(),
+                    new Length([
+                        'min' => 4,
+                        'max' => 255
+
+                    ])
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => "Choisisser un mot de passe",
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 4,
+                        'max' => 255
+                    ])
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => "Prénom",
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire",
+                'validate' => false,
+                'attr' => [
+                    'class' => 'd-block mx-auto my-3 col-4 btn btn-primary',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
